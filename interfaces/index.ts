14446db6095b11f77632bed77dd3aabc181db6e7
Senal <senal.dolage@gmail.com>
2022-05-16 20:47:00 +0530

    new show type added, movie type updates

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -4,15 +4,44 @@ export type Movie = {
   adult: boolean;
   backdrop_path: string;
   genre_ids: number[];
-  id: number | string;
+  genres: [{ id: number; name: string }];
+  id: number;
   original_language: string;
   original_title: string;
+  original_name: string;
+  first_air_date: string;
+  runtime: number;
   overview: string;
   popularity: number;
   poster_path: string;
-  release_date: string | Date;
+  release_date: string;
   title: string;
-  video: false;
+  video: boolean;
   vote_average: number;
   vote_count: number;
+  videos: any;
+  images: any;
 };
+
+export type Show = { 
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  genres: [{ id: number; name: string }];
+  id: number;
+  original_language: string;
+  original_title: string;
+  original_name: string;
+  first_air_date: string;
+  runtime: number;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+  videos: any;
+  images: any;
+}
