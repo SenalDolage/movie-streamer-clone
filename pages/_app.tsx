52615c3d748ea5357acd03b216c6b8ee464c8a7d
Senal <senal.dolage@gmail.com>
2022-05-16 20:47:18 +0530

    import order change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react';
 
 import '../styles/index.css'
-import { SessionProvider } from 'next-auth/react';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
