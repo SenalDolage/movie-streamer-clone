ed431d19a41c15ebe90541f8085e4b94ee19150a
Senal <senal.dolage@gmail.com>
2022-05-16 20:44:30 +0530

    header responsive and navbar updates

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,19 +1,14 @@
 import Link from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
-import {
-    HomeIcon,
-    SearchIcon,
-    PlusIcon,
-    StarIcon,
-} from "@heroicons/react/solid";
+import { HomeIcon } from "@heroicons/react/solid";
 import { useSession, signIn, signOut } from "next-auth/react"
 
-type Props = {
+type HeaderProps = {
     title?: string
 }
 
-export const Header = ({ title = 'Movie Streamer Clone' }: Props) => {
+export const Header = ({ title = 'Movie Streamer Clone' }: HeaderProps) => {
     const { data: session } = useSession();
 
     return (
@@ -25,8 +20,8 @@ export const Header = ({ title = 'Movie Streamer Clone' }: Props) => {
             </Head>
 
             <header className="sticky bg-primary w-full top-0 left-0 z-10 ">
-                <div className="header-inner max-w-screen-2xl mx-auto flex items-center justify-between px-8 xl:px-0">
-                    <div className="logo">
+                <div className="header-inner max-w-screen-xl mx-auto flex items-center justify-between px-8 py-5 md:py-0 flex-wrap md:flex-nowrap">
+                    <div className="logo w-full text-center md:w-auto">
                         <a href="/">
                             <Image
                                 src="/images/logo.svg"
@@ -47,35 +42,14 @@ export const Header = ({ title = 'Movie Streamer Clone' }: Props) => {
                                     </a>
                                 </Link>
 
-                                <Link href="/a">
-                                    <a className="header-link group">
-                                        <SearchIcon className="h-4" />
-                                        <span className="span">Search</span>
-                                    </a>
-                                </Link>
-
-                                <Link href="/aa">
-                                    <a className="header-link group">
-                                        <PlusIcon className="h-4" />
-                                        <span className="span">Watchlist</span>
-                                    </a>
-                                </Link>
-
-                                <Link href="/aa">
-                                    <a className="header-link group">
-                                        <StarIcon className="h-4" />
-                                        <span className="span">Originals</span>
-                                    </a>
-                                </Link>
-
-                                <Link href="/de">
+                                <Link href="/">
                                     <a className="header-link group">
                                         <img src="/images/movie-icon.svg" alt="" className="h-5" />
                                         <span className="span">Movies</span>
                                     </a>
                                 </Link>
 
-                                <Link href="/de">
+                                <Link href="/">
                                     <a className="header-link group">
                                         <img src="/images/series-icon.svg" alt="" className="h-5" />
                                         <span className="span">Series</span>
@@ -85,28 +59,28 @@ export const Header = ({ title = 'Movie Streamer Clone' }: Props) => {
                         </div>
                         : ''}
 
-                    <div className="login-wrap">
+                    <div className="login-wrap w-full md:w-auto">
                         {session ?
-                            <>
-                                <div className="flex items-center space-x-6">
-                                    <span>
-                                        Hello {session.user.name}!
-                                    </span>
-                                    {session.user.image ?
-                                        <img
-                                            src={session.user.image}
-                                            alt="user"
-                                            className="h-12 w-12 rounded-full object-cover"
-                                        />
-                                        : ''
-                                    }
-                                    <button className="uppercase border px-4 py-1.5 rounded font-medium tracking-wide hover:bg-white hover:text-primary transition duration-200" onClick={() => signOut()}>Log Out</button>
-                                </div>
-                            </>
+                            <div className="flex items-center justify-center space-x-6">
+                                <span>
+                                    Hello {session.user.name}!
+                                </span>
+                                {session.user.image ?
+                                    <img
+                                        src={session.user.image}
+                                        alt="user"
+                                        className="h-12 w-12 rounded-full object-cover hidden lg:inline-block"
+                                    />
+                                    : ''
+                                }
+                                <button className="uppercase border px-4 py-1.5 rounded font-medium tracking-wide hover:bg-white hover:text-primary transition duration-200" onClick={() => signOut()}>
+                                    Log Out
+                                </button>
+                            </div>
                             :
-                            <>
-                                <button className="uppercase border px-4 py-1.5 rounded font-medium tracking-wide hover:bg-white hover:text-primary transition duration-200" onClick={() => signIn()}>Login</button>
-                            </>
+                            <button className="uppercase border px-4 py-1.5 rounded font-medium tracking-wide hover:bg-white hover:text-primary transition duration-200" onClick={() => signIn()}>
+                                Login
+                            </button>
                         }
                     </div>
                 </div>
